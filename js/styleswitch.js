c036be529bd034d30781919563343fe7fd368c9d
ocket8888 <ocket8888@gmail.com>
2018-05-07 16:18:36 -0600

    fixed 'night' class being given to 'head' elems

--- js/styleswitch.js
+++ js/styleswitch.js
@@ -9,14 +9,19 @@ window.onload = () => {
 	for (var i = cookies.length - 1; i >= 0; i--) {
 		if (cookies[i].split("=")[0] === "night") {
 			NightMode = cookies[i].split("=")[1] === "true";
+			break;
 		}
 	};
 
 	/* Record list of page elements to apply styles later */
-	allElements = document.querySelectorAll("*");
+	allElements = document.querySelectorAll("body, body *");
+
 	nightmodeButtonText = document.getElementById('nightmodeButton').childNodes[0];
 	if (NightMode) {
 		nightmodeButtonText.data = "Day Mode";
+		for (var i = allElements.length - 1; i>=0; i--) {
+			allElements[i].classList.add("night");
+		}
 	}
 }
 
